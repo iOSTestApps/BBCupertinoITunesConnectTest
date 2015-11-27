39455b2269543b59f33b64a7ae390a61bff5e7b8
Christopher Stott <christopherstott@gmail.com>
2015-11-27 14:36:31 -0800

    Initial

---
+++ BBCupertinoITunesConnectTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BBCupertinoITunesConnectTest
+//
+//  Created by Chris on 2015-11-27.
+//  Copyright © 2015 Chris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
